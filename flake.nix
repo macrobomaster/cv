1b4e0d2abcffe4252c64b632a20486537c0f62f3
Woze Parrot <wozeparrot@gmail.com>
2025-01-30 21:27:22 -0500

    feat: move to separate disk.nix

--- flake.nix
+++ flake.nix
@@ -107,43 +107,8 @@
             inputs.jetpack-nixos.nixosModules.default
             inputs.disko.nixosModules.disko
             ./nix/nixos/base.nix
+            ./nix/nixos/disk.nix
           ];
-
-          disko.devices.disk.main = {
-            device = "/dev/nvme0n1";
-            type = "disk";
-            content = {
-              type = "gpt";
-              partitions = {
-                ESP = {
-                  end = "1G";
-                  type = "EF00";
-                  content = {
-                    type = "filesystem";
-                    format = "vfat";
-                    mountpoint = "/boot";
-                    mountOptions = [ "umask=0077" ];
-                  };
-                };
-                root = {
-                  name = "root";
-                  end = "-0";
-                  content = {
-                    type = "filesystem";
-                    format = "f2fs";
-                    mountpoint = "/";
-                    extraArgs = [
-                      "-O"
-                      "extra_attr,inode_checksum,sb_checksum,compression"
-                    ];
-                    mountOptions = [
-                      "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
-                    ];
-                  };
-                };
-              };
-            };
-          };
 
           boot.loader.systemd-boot.enable = true;
           boot.loader.efi.canTouchEfiVariables = true;
---
+++ nix/nixos/disk.nix
@@ -0,0 +1,37 @@
+{
+  disko.devices.disk.main = {
+    device = "/dev/nvme0n1";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          end = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        root = {
+          name = "root";
+          end = "-0";
+          content = {
+            type = "filesystem";
+            format = "f2fs";
+            mountpoint = "/";
+            extraArgs = [
+              "-O"
+              "extra_attr,inode_checksum,sb_checksum,compression"
+            ];
+            mountOptions = [
+              "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
