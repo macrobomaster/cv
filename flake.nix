aae09992981a6a071f7edd35748ebe7a2116b150
Woze Parrot <wozeparrot@gmail.com>
2025-01-31 19:53:43 -0500

    feat: still need systemd

--- flake.nix
+++ flake.nix
@@ -116,11 +116,8 @@
           hardware.enableRedistributableFirmware = true;
           boot.initrd.availableKernelModules = [ "nvme" "f2fs" "pcie-tegra194" ];
           boot.supportedFilesystems = [ "f2fs" "vfat" ];
-          # boot.loader.systemd-boot.enable = true;
-          # boot.loader.efi.canTouchEfiVariables = true;
-          boot.loader.grub.enable = true;
-          boot.loader.grub.efiSupport = true;
-          boot.loader.grub.efiInstallAsRemovable = true;
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
 
           hardware.enableAllHardware = lib.mkForce false;
           hardware.nvidia-jetpack = {
