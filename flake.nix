338ddc3676e875e6c3e668026d4b39621b193d69
Woze Parrot <wozeparrot@gmail.com>
2025-02-02 21:23:34 -0500

    feat: enable graphics

--- flake.nix
+++ flake.nix
@@ -190,6 +190,7 @@
           boot.loader.systemd-boot.enable = true;
           boot.loader.efi.canTouchEfiVariables = true;
 
+          hardware.graphics.enable = true;
           hardware.nvidia-jetpack = {
             enable = true;
             som = "orin-nano";
