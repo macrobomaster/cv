b703e3e7c5818bc96fbf144531239c7249bef194
Woze Parrot <wozeparrot@gmail.com>
2025-02-04 10:08:01 -0500

    feat: tinygrad bgr to yuv420

--- cv/common/image.py
+++ cv/common/image.py
@@ -1,3 +1,5 @@
+from tinygrad.tensor import Tensor
+from tinygrad.dtype import dtypes
 import cv2
 import numpy as np
 
@@ -17,3 +19,31 @@ def bgr_to_yuv420(img):
 
   # stack the channels
   return np.stack([y0, y1, y2, y3, u, v], axis=-1)
+
+def bgr_to_yuv420_tensor(img:Tensor) -> Tensor:
+  coeffs = Tensor([[0.299, 0.587, 0.114],
+                   [-0.168736, -0.331264, 0.5],
+                   [0.5, -0.418688, -0.081312]]).reshape(3, 3)
+  yuv = img[:, :, :, ::-1].matmul(coeffs.T)
+  yuv = yuv.add(Tensor([0, 128, 128]))
+
+  y0 = yuv[:, ::2, ::2, 0]
+  y1 = yuv[:, 1::2, ::2, 0]
+  y2 = yuv[:, ::2, 1::2, 0]
+  y3 = yuv[:, 1::2, 1::2, 0]
+  u = yuv[:, :, :, 1].avg_pool2d(2, 2)
+  v = yuv[:, :, :, 2].avg_pool2d(2, 2)
+
+  return Tensor.stack(y0, y1, y2, y3, u, v, dim=-1).clamp(0, 255).cast(dtypes.uint8)
+
+if __name__ == "__main__":
+  img = cv2.imread("test.png")
+  img = cv2.resize(img, (812, 1080))
+
+  yuv_tensor = bgr_to_yuv420_tensor(Tensor(img).unsqueeze(0))
+  yuv = yuv_tensor.numpy()[0]
+  yuv_ref = bgr_to_yuv420(img)
+  for i in range(6):
+    # put reference next to our implementation
+    cv2.imshow("yuv", np.hstack([yuv_ref[..., i], yuv[..., i]]))
+    cv2.waitKey(0)
