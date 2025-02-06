7b15e2d6717a6608abbc0285a2d3d377431c6fa6
Woze Parrot <wozeparrot@gmail.com>
2025-02-06 15:36:39 -0500

    feat: model tweaks

--- cv/autoaim/model.py
+++ cv/autoaim/model.py
@@ -3,7 +3,9 @@ from tinygrad.dtype import dtypes
 from tinygrad.tensor import Tensor
 
 from ..common.tensor import channel_shuffle, pixel_unshuffle, telu
-from ..common.nn import BatchNorm, ConvNorm, Attention, SE
+from ..common.nn import BatchNorm, ConvNorm, Attention, SE, SRM, RecConv
+
+def nonlinear(x:Tensor) -> Tensor: return x.gelu()
 
 class TokenMixer:
   def __init__(self, dim:int, stride:int=1, attn:bool=False):
@@ -11,45 +13,57 @@ class TokenMixer:
     assert stride in [1, 2], "stride must be 1 or 2"
     self.stride, self.has_attn = stride, attn
 
+    # complex_dim = dim // 4
+    # simple_dim = (dim * 3) // 4
+
+    complex_dim = dim // 2
+    simple_dim = dim // 2
+
     if self.has_attn:
-      self.attn = Attention(dim // 4, min(16, dim // 4), heads=1)
+      self.attn = Attention(complex_dim, min(16, complex_dim), heads=1)
     else:
-      self.conv7x7 = nn.Conv2d(dim // 4, dim // 4, 7, stride, 3, groups=dim // 4, bias=True)
-    self.conv3x3 = nn.Conv2d(dim // 4, dim // 4, 3, stride, 1, groups=dim // 4, bias=True)
-    self.conv2x2 = nn.Conv2d(dim // 4, dim // 4, 2, stride, 1, dilation=2, groups=dim // 4, bias=True)
+      if self.stride == 1:
+        self.rec = RecConv(complex_dim, 5, levels=2)
+      else:
+        self.conv7x7 = nn.Conv2d(complex_dim, complex_dim, 7, stride, 3, groups=complex_dim, bias=False)
+
+    self.conv3x3 = nn.Conv2d(simple_dim, simple_dim, 3, stride, 1, groups=simple_dim, bias=False)
 
   def __call__(self, x:Tensor) -> Tensor:
-    x0, x1, x2, x3 = channel_shuffle(x, 4)
+    # # 1/4, 3/4 channel shuffle split
+    # x0, x1, x2, x3 = channel_shuffle(x, 4)
+    # x1 = x1.cat(x2, x3, dim=1)
 
+    # 1/2, 1/2 channel shuffle split
+    x0, x1 = channel_shuffle(x, 2)
+
+    # complex mixer
     if self.has_attn:
-      b, c, h, w = x1.shape
-      x1 = x1.flatten(2).transpose(1, 2)
-      x1 = self.attn(x1).transpose(1, 2).reshape(b, c, h, w)
-    else:
-      x1 = self.conv7x7(x1)
-    x2 = self.conv3x3(x2)
-    x3 = self.conv2x2(x3)
-
-    # shortcut
-    if self.stride == 2:
-      x0 = pixel_unshuffle(x0, 2)
       b, c, h, w = x0.shape
-      x0 = x0.reshape(b, x1.shape[1], c // x1.shape[1], h, w)
-      x0 = x0.mean(2)
+      x0 = x0.flatten(2).transpose(1, 2)
+      x0 = self.attn(x0).transpose(1, 2).reshape(b, c, h, w)
+    else:
+      if self.stride == 1:
+        x0 = self.rec(x0)
+      else:
+        x0 = self.conv7x7(x0)
 
-    return x0.cat(x1, x2, x3, dim=1)
+    # simple mixer
+    x1 = self.conv3x3(x1)
+
+    return x0.cat(x1, dim=1)
 
 class ChannelMixer:
   def __init__(self, cin:int, cout:int=0):
-    self.proj = nn.Conv2d(cin, cin * 2, 1, 1, 0, bias=True)
-    self.mix = nn.Conv2d(cin * 2, cin * 2, 3, 1, 1, groups=cin * 2, bias=True)
-    self.out = nn.Conv2d(cin, cin if cout == 0 else cout, 1, 1, 0, bias=True)
+    self.proj = nn.Conv2d(cin, cin * 2, 1, 1, 0, bias=False)
+    self.mix = nn.Conv2d(cin * 2, cin * 2, 3, 1, 1, groups=cin * 2, bias=False)
+    self.out = nn.Conv2d(cin, cin if cout == 0 else cout, 1, 1, 0, bias=False)
 
   def __call__(self, x:Tensor) -> Tensor:
     x = self.proj(x)
 
     x, gate = self.mix(x).chunk(2, dim=1)
-    x = x * telu(gate)
+    x = x * nonlinear(gate)
 
     return self.out(x)
 
@@ -89,12 +103,13 @@ class Stem:
   def __init__(self, cin:int, cout:int):
     self.conv1 = ConvNorm(cin, cout // 2, 3, 2, 1, bias=False)
     self.conv2 = ConvNorm(cout // 2, cout, 3, 2, 1, bias=False)
-    self.se = SE(cout, max(4, cout // 16))
+    # self.se = SE(cout, max(4, cout // 16))
+    self.srm = SRM(cout)
 
   def __call__(self, x: Tensor) -> Tensor:
-    x = telu(self.conv1(x))
+    x = nonlinear(self.conv1(x))
     x = self.conv2(x)
-    return self.se(x)
+    return self.srm(x)
 
 class Stage:
   def __init__(self, cin:int, cout:int, num_blocks:int, attn:bool=False):
@@ -106,13 +121,13 @@ class Stage:
     return x.sequential(self.blocks)
 
 class Backbone:
-  def __init__(self, cin:int=3, cstage:list[int]=[16, 32, 64, 128], stages:list[int]=[2, 2, 5, 1]):
+  def __init__(self, cin:int, cstage:list[int], stages:list[int], attn:bool):
     self.stem = Stem(cin, cstage[0])
 
     self.stage0 = Stage(cstage[0], cstage[0], stages[0])
     self.stage1 = Stage(cstage[0], cstage[1], stages[1])
-    self.stage2 = Stage(cstage[1], cstage[2], stages[2], attn=True)
-    self.stage3 = Stage(cstage[2], cstage[3], stages[3], attn=True)
+    self.stage2 = Stage(cstage[1], cstage[2], stages[2], attn=attn)
+    self.stage3 = Stage(cstage[2], cstage[3], stages[3], attn=attn)
 
   def __call__(self, x:Tensor) -> tuple[Tensor, Tensor, Tensor, Tensor]:
     x = self.stem(x)
@@ -129,78 +144,71 @@ class FFNBlock:
     self.up = nn.Linear(dim, exp_dim)
     self.down = nn.Linear(exp_dim, dim * 2)
   def __call__(self, x:Tensor) -> Tensor:
-    xx = telu(self.up(x))
+    xx = nonlinear(self.up(x))
     xx, gate = self.down(xx).chunk(2, dim=-1)
-    xx = xx * telu(gate)
-    return telu(x + xx)
+    xx = xx * nonlinear(gate)
+    return x + xx
 
 class FFN:
-  def __init__(self, in_dim:int, mid_dim:int, out_dim:int, exp_dim:int=0, blocks:int=1):
+  def __init__(self, in_dim:int, out_dim:int, mid_dim:int, exp_dim:int=0, blocks:int=1):
     if exp_dim == 0: exp_dim = mid_dim
     self.proj = nn.Linear(in_dim, mid_dim)
     self.blocks = [FFNBlock(mid_dim, exp_dim) for _ in range(blocks)]
     self.out = nn.Linear(mid_dim, out_dim)
 
   def __call__(self, x:Tensor) -> Tensor:
-    x = telu(self.proj(x))
+    x = nonlinear(self.proj(x))
     x = x.sequential(self.blocks)
     return self.out(x)
 
 class Neck:
   def __init__(self, cins:list[int], cout:int, cmid:int=256):
-    self.x0 = ChannelMixer(cins[0], cmid)
-    self.x1 = ChannelMixer(cins[1], cmid)
-    self.x2 = ChannelMixer(cins[2], cmid)
-    self.x3 = ChannelMixer(cins[3], cmid)
+    # self.x0norm = BatchNorm(cins[0])
+    # self.x0 = ChannelMixer(cins[0], cmid)
+    # self.x1norm = BatchNorm(cins[1])
+    # self.x1 = ChannelMixer(cins[1], cmid)
+    # self.x2norm = BatchNorm(cins[2])
+    # self.x2 = ChannelMixer(cins[2], cmid)
+    # self.x3norm = BatchNorm(cins[3])
+    # self.x3 = ChannelMixer(cins[3], cmid)
+    self.x2 = ConvNorm(cins[2], cmid, 1, 1, 0, bias=False)
+    self.x3 = ConvNorm(cins[3], cmid, 1, 1, 0, bias=False)
 
-    self.features = cout // cmid
-    self.feature = Tensor.kaiming_normal(1, self.features, cmid)
-
-    self.attn_norm = BatchNorm(cmid)
-    self.attn = Attention(cmid, cmid, heads=2)
-    self.ffn_norm = BatchNorm(cmid)
-    self.ffn = FFN(cmid, cmid, cmid, exp_dim=cmid*2)
+    self.out_norm = BatchNorm(cmid*2)
+    self.out = FFN(cmid*2, cout, cmid, exp_dim=cmid*2, blocks=2)
 
   def __call__(self, xs:tuple[Tensor, Tensor, Tensor, Tensor]) -> Tensor:
     x0, x1, x2, x3 = xs
-    x0 = self.x0(x0).mean((2, 3))
-    x1 = self.x1(x1).mean((2, 3))
+    # x0 = self.x0(self.x0norm(x0)).mean((2, 3))
+    # x1 = self.x1(self.x1norm(x1)).mean((2, 3))
+    # x2 = self.x2(self.x2norm(x2)).mean((2, 3))
+    # x3 = self.x3(self.x3norm(x3)).mean((2, 3))
     x2 = self.x2(x2).mean((2, 3))
     x3 = self.x3(x3).mean((2, 3))
 
-    x = Tensor.stack(x0, x1, x2, x3, dim=1)
+    x = x2.cat(x3, dim=1)
 
-    # concat with feature
-    x = x.cat(self.feature.expand(x.shape[0], self.features, -1), dim=1)
-
-    # attention
-    xx = self.attn(self.attn_norm(x.transpose(1, 2)).transpose(1, 2))
-    x = x + xx
-
-    # ffn
-    x = x[:, -self.features:]
-    xx = self.ffn(self.ffn_norm(x.transpose(1, 2)).transpose(1, 2))
-    x = x + xx
-    return x.flatten(1)
+    # project out
+    return self.out(self.out_norm(x))
 
 class Head:
   def __init__(self, in_dim:int, mid_dim:int, out_dim:int):
-    self.ffn = FFN(in_dim, mid_dim, out_dim)
+    self.ffn = FFN(in_dim, mid_dim, out_dim, blocks=1)
 
   def __call__(self, x:Tensor) -> Tensor:
     return self.ffn(x)
 
 class Model:
-  def __init__(self, mid:int=512, head:int=64, cstage:list[int]=[16, 32, 64, 128], stages:list[int]=[2, 2, 5, 2]):
+  def __init__(self, mid:int=256, head:int=64, cstage:list[int]=[16, 32, 64, 128], stages:list[int]=[2, 2, 6, 2]):
     # feature extractor
-    self.backbone = Backbone(cin=6, cstage=cstage, stages=stages)
+    self.backbone = Backbone(cin=6, cstage=cstage, stages=stages, attn=True)
     self.neck = Neck(cstage, mid)
 
     # heads
-    self.cls_head = Head(mid, head, 2)
-    self.x_head = Head(mid, head, 512)
-    self.y_head = Head(mid, head, 256)
-    self.dist_head = Head(mid, head, 200)
+    self.cls_head = Head(mid, 2, head)
+    self.x_head = Head(mid, 512, head)
+    self.y_head = Head(mid, 256, head)
+    self.dist_head = Head(mid, 256, head)
 
   def __call__(self, img:Tensor):
     # image normalization
@@ -219,7 +227,7 @@ class Model:
       cl = (cl[0].sigmoid().argmax(1), cl[0].sigmoid()[:, cl[0].argmax(1)])
       x = (x.softmax() @ Tensor.arange(512)).float()
       y = (y.softmax() @ Tensor.arange(256)).float()
-      dist = ((dist.softmax() @ Tensor.arange(200)) / 10).float()
+      dist = ((dist.softmax() @ Tensor.arange(256)) / 16).float()
 
     return *cl, x, y, dist
 
