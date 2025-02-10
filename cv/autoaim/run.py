6e5a6d549ed34686dc7dd78711a8bbea38744de1
Woze Parrot <wozeparrot@gmail.com>
2025-02-10 18:19:09 -0500

    feat: not really faster

--- cv/autoaim/run.py
+++ cv/autoaim/run.py
@@ -20,7 +20,7 @@ def frame_thread_fn(stop_event: threading.Event, frame_queue: Queue):
     sft = time.perf_counter()
     img = get_aravis_frame(cam, strm)
     # resize and convert to yuv
-    img = cv2.resize(img, (512, 256), interpolation=cv2.INTER_NEAREST)
+    img = cv2.resize(img, (512, 256))
     ft = time.perf_counter() - sft
     frame_queue.put((img, ft))
 
