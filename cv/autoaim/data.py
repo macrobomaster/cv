c702de9f73cfd2b64d25eadb625ecca0dc20ccb8
Woze Parrot <wozeparrot@gmail.com>
2025-02-16 21:18:34 -0500

    feat: move load_single_file out of train.py

---
+++ cv/autoaim/data.py
@@ -0,0 +1,101 @@
+import glob
+
+from tinygrad.tensor import _to_np_dtype
+from tinygrad.device import Device
+from tinygrad.dtype import dtypes
+import cv2
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
+import pyvips
+import numpy as np
+import albumentations as A
+
+from ..common import BASE_PATH
+from .common import get_annotation
+from .syndata import generate_sample
+
+def get_train_files():
+  real_files = glob.glob(str(BASE_PATH / "data" / "**" / "*.png"), recursive=True)
+  real_files = [f"path:{f}" for f in real_files]
+  fake_files = ["fake:3", "fake:4", "fake:5"] * len(real_files)
+  return real_files + fake_files
+
+OUTPUT_PIPELINE = None
+def load_single_file(file):
+  global OUTPUT_PIPELINE
+  if OUTPUT_PIPELINE is None:
+    OUTPUT_PIPELINE = A.Compose([
+      A.HorizontalFlip(p=0.5),
+      A.Perspective(p=0.25),
+      A.Affine(translate_percent=(-0.2, 0.2), scale=(0.9, 1.1), rotate=(-45, 45), shear=(-5, 5), border_mode=cv2.BORDER_CONSTANT, fill=0, p=0.5),
+      A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+      A.HueSaturationValue(hue_shift_limit=5, sat_shift_limit=30, val_shift_limit=20, p=0.5),
+      A.RGBShift(r_shift_limit=10, g_shift_limit=10, b_shift_limit=10, p=0.5),
+      A.OneOf([
+        A.RandomGamma(gamma_limit=(80, 120), p=0.5),
+        A.RandomToneCurve(p=0.5),
+      ], p=0.2),
+    ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False))
+
+  if file.startswith("path:"):
+    img = pyvips.Image.new_from_file(file[5:], access="sequential").numpy()
+    img = img[..., :3]
+    if img.shape[0] != 256 or img.shape[1] != 512:
+      img = cv2.resize(img, (512, 256))
+
+    anno = get_annotation(file[5:])
+    detected, x, y, dist = anno.detected, anno.x, anno.y, anno.dist
+
+    # transform points
+    x, y = x * (img.shape[1] - 1), (1 - y) * (img.shape[0] - 1)
+
+    # augment
+    transformed = OUTPUT_PIPELINE(image=img, keypoints=[(x, y)])
+    img, x, y = transformed["image"], transformed["keypoints"][0][0], transformed["keypoints"][0][1]
+    # see if point is still within image
+    if x < 0 or x > img.shape[1] or y < 0 or y > img.shape[0]:
+      detected = 0
+
+    return {
+      "x": img.tobytes(),
+      "y": np.array((detected, x, y, dist), dtype=_to_np_dtype(dtypes.default_float)).tobytes(),
+    }
+  elif file.startswith("fake:"):
+    img, x, y = generate_sample(file)
+
+  output = OUTPUT_PIPELINE(image=img, keypoints=[(x, y)])
+  img = output["image"]
+  x, y = output["keypoints"][0]
+  if x < 0 or x > img.shape[1] or y < 0 or y > img.shape[0]:
+    detected = 0
+  else:
+    detected = 1
+
+  return {
+    "x": img.tobytes(),
+    "y": np.array((detected, x, y, 0), dtype=_to_np_dtype(dtypes.default_float)).tobytes(),
+  }
+
+def single_batch(iter):
+  d, c = next(iter)
+  return d["x"].to(Device.DEFAULT), d["y"].to(Device.DEFAULT), c
+
+if __name__ == "__main__":
+  from tinygrad.helpers import tqdm
+  from ..common.dataloader import batch_load, BatchDesc
+
+  BS = 256
+
+  batch_iter = iter(tqdm(batch_load(
+    {
+      "x": BatchDesc(shape=(256, 512, 3), dtype=dtypes.uint8),
+      "y": BatchDesc(shape=(4,), dtype=dtypes.default_float),
+    },
+    load_single_file, get_train_files, bs=BS, shuffle=True,
+  ), total=len(get_train_files())//BS))
+
+  i, proc = 0, single_batch(batch_iter)
+  while proc is not None:
+    try: next_proc = single_batch(batch_iter)
+    except StopIteration: next_proc = None
+    proc, next_proc = next_proc, None
--- cv/autoaim/train.py
+++ cv/autoaim/train.py
@@ -1,139 +1,56 @@
-import math, time, glob, random
+import math, time
 
-from tinygrad.tensor import Tensor, _to_np_dtype
+from tinygrad.tensor import Tensor
 from tinygrad.dtype import dtypes
 from tinygrad.nn.state import get_parameters, get_state_dict, load_state_dict, safe_load, safe_save
-from tinygrad.device import Device
 from tinygrad.engine.jit import TinyJit
 from tinygrad.helpers import GlobalCounters, tqdm, trange, getenv
 import wandb
 import cv2
 cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
-import numpy as np
-import albumentations as A
 
 from .model import Model
-from .common import get_annotation
+from .data import get_train_files, load_single_file, single_batch
 from ..common.tensor import twohot, masked_cross_entropy, mal_loss
 from ..common import BASE_PATH
 from ..common.optim import CLAMB
-from ..common.image import bgr_to_yuv420_tensor, alpha_overlay
+from ..common.image import rgb_to_yuv420_tensor
 from ..common.dataloader import batch_load, BatchDesc
 
-def get_train_files():
-  real_files = glob.glob(str(BASE_PATH / "data" / "**" / "*.png"), recursive=True)
-  real_files = [f"path:{f}" for f in real_files]
-  fake_files = ["fake:4"] * len(real_files)
-  return real_files + fake_files
-
-def load_single_file(file):
-  OUTPUT_PIPELINE = A.Compose([
-    A.HorizontalFlip(p=0.5),
-    A.Perspective(p=0.25),
-    A.Affine(translate_percent=(-0.2, 0.2), scale=(0.9, 1.1), rotate=(-45, 45), shear=(-5, 5), border_mode=cv2.BORDER_CONSTANT, fill=0, p=0.5),
-    A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
-    A.HueSaturationValue(hue_shift_limit=5, sat_shift_limit=30, val_shift_limit=20, p=0.5),
-    A.RGBShift(r_shift_limit=10, g_shift_limit=10, b_shift_limit=10, p=0.5),
-    A.OneOf([
-      A.RandomGamma(gamma_limit=(80, 120), p=0.5),
-      A.RandomToneCurve(p=0.5),
-    ], p=0.2),
-  ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False))
-
-  if file.startswith("path:"):
-    img = cv2.imread(file[5:])
-    if img.shape[0] != 256 or img.shape[1] != 512:
-      img = cv2.resize(img, (512, 256))
-
-    anno = get_annotation(file[5:])
-    detected, x, y, dist = anno.detected, anno.x, anno.y, anno.dist
-
-    # transform points
-    x, y = x * (img.shape[1] - 1), (1 - y) * (img.shape[0] - 1)
-
-    # augment
-    transformed = OUTPUT_PIPELINE(image=img, keypoints=[(x, y)])
-    img, x, y = transformed["image"], transformed["keypoints"][0][0], transformed["keypoints"][0][1]
-    # see if point is still within image
-    if x < 0 or x > img.shape[1] or y < 0 or y > img.shape[0]:
-      detected = 0
-
-    return {
-      "x": img.tobytes(),
-      "y": np.array((detected, x, y, dist), dtype=_to_np_dtype(dtypes.default_float)).tobytes(),
-    }
-  elif file.startswith("fake:"):
-    PLATE_PIPELINE = A.Compose([
-      A.RandomScale(scale_limit=(0.002-1, 0.02-1), p=1),
-      A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=0.5),
-      A.Perspective(scale=(0.05, 0.2), keep_size=True, fit_output=True, p=1),
-    ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False))
-    BACKGROUND_PIPELINE = A.Compose([
-      A.RandomResizedCrop(size=(256, 512), scale=(0.1, 1.0), ratio=(1.9, 2.1), p=1),
-    ])
-
-    if getattr(load_single_file, "raw_plate", None) is None:
-      setattr(load_single_file, "raw_plate", cv2.imread(str(BASE_PATH / "armor_plate" / "4_front.png"), cv2.IMREAD_UNCHANGED))
-    if getattr(load_single_file, "raw_background", None) is None:
-      setattr(load_single_file, "raw_background", cv2.imread(str(BASE_PATH / "background" / "room.png")))
-
-    raw_plate = getattr(load_single_file, "raw_plate")
-    raw_background = getattr(load_single_file, "raw_background")
-
-    plate_out = PLATE_PIPELINE(image=raw_plate, keypoints=[(raw_plate.shape[1]//2, raw_plate.shape[0]//2)])
-    plate = plate_out["image"]
-    img = BACKGROUND_PIPELINE(image=raw_background)["image"]
-
-    # put the plate on the background with alpha blending
-    x = random.randint(0, img.shape[1] - plate.shape[1])
-    y = random.randint(0, img.shape[0] - plate.shape[0])
-    alpha_overlay(plate, img, x, y)
-
-    x += plate_out["keypoints"][0][0]
-    y += plate_out["keypoints"][0][1]
-
-  output = OUTPUT_PIPELINE(image=img, keypoints=[(x, y)])
-  img = output["image"]
-  x, y = output["keypoints"][0]
-  if x < 0 or x > img.shape[1] or y < 0 or y > img.shape[0]:
-    detected = 0
-  else:
-    detected = 1
-
-  return {
-    "x": img.tobytes(),
-    "y": np.array((detected, x, y, 0), dtype=_to_np_dtype(dtypes.default_float)).tobytes(),
-  }
-
 BS = 256
-WARMUP_STEPS = 100
+WARMUP_STEPS = 200
 WARMPUP_LR = 1e-7
 START_LR = 1e-3
 END_LR = 1e-5
-EPOCHS = 50
+EPOCHS = 30
 STEPS_PER_EPOCH = len(get_train_files())//BS
 
 def loss_fn(pred: tuple[Tensor, Tensor, Tensor, Tensor], y: Tensor):
   det_gate = (y[:, 0] > 0).detach()
 
-  x_loss = masked_cross_entropy(pred[1], twohot(y[:, 1] / 4, 128), det_gate)
-  y_loss = masked_cross_entropy(pred[2], twohot(y[:, 2] / 4, 64), det_gate)
+  x_loss = masked_cross_entropy(pred[1], twohot(y[:, 1], 512), det_gate)
+  y_loss = masked_cross_entropy(pred[2], twohot(y[:, 2], 256), det_gate)
 
   # dist_loss = masked_cross_entropy(pred[3], twohot(y[:, 3] * 4, 64), (det_gate & (y[:, 3] > 0)).detach())
 
-  point_x = (pred[1].softmax() @ Tensor.arange(128)).float() / 128
-  point_y = (pred[2].softmax() @ Tensor.arange(64)).float() / 64
-  point_dist = (point_x.sub(y[:, 1] / 512).square() + point_y.sub(y[:, 2] / 256).square()).sqrt()
+  if not hasattr(loss_fn, "x_arange"): setattr(loss_fn, "x_arange", Tensor.arange(512))
+  if not hasattr(loss_fn, "y_arange"): setattr(loss_fn, "y_arange", Tensor.arange(256))
+  point_x = (pred[1].softmax() @ getattr(loss_fn, "x_arange")).float() / 512
+  point_y = (pred[2].softmax() @ getattr(loss_fn, "y_arange")).float() / 256
+  point_dist = (point_x.sub(y[:, 1] / 512).square() + point_y.sub(y[:, 2] / 256).square())
   point_loss = det_gate.where(point_dist, 0).sum() / det_gate.sum().add(1e-6)
 
-  cl_loss = mal_loss(pred[0], y[:, 0].cast(dtypes.int32), (1 - point_dist.clamp(0, 1)).unsqueeze(-1), gamma=1.5)
+  target_cls = y[:, 0].cast(dtypes.int32).one_hot(2)
+  quality = (1 - point_dist.sqrt().clamp(0, 1))
+  quality = target_cls[:, 0].stack(quality, dim=1)
+  cl_loss = mal_loss(pred[0], target_cls, quality, gamma=1.5)
 
   return cl_loss + x_loss + y_loss + point_loss
 
 @TinyJit
 def train_step(x, y, lr):
-  yuv = bgr_to_yuv420_tensor(x)
+  yuv = rgb_to_yuv420_tensor(x)
 
   pred = model(yuv)
   loss = loss_fn(pred, y)
@@ -188,10 +105,6 @@ if __name__ == "__main__":
 
   parameters = get_parameters(model)
   optim = CLAMB(parameters, weight_decay=0.1, b1=0.9, b2=0.994, adam=True)
-
-  def single_batch(iter):
-    d, c = next(iter)
-    return d["x"].to(Device.DEFAULT), d["y"].to(Device.DEFAULT), c
 
   steps = 0
   for epoch in trange(EPOCHS):
