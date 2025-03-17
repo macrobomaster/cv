26b4dd2129ab9d08f9b9ef811104eaa4901f43fd
Woze Parrot <wozeparrot@gmail.com>
2025-03-17 14:07:06 -0400

    feat: training script

---
+++ cv/autoaim/train.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export BASE_PATH=/mnt/tmp/ai/cv-e2e-playground/
+
+export BEAM=2
+export IGNORE_JIT_FIRST_BEAM=1
+export BEAM_MIN_PROGRESS=5
+export BEAM_UOPS_MAX=3000
+
+python -m cv.autoaim.train
