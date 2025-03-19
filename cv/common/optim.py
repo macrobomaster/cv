a0666b8c4aefcba61b1f761a946e946975563337
Woze Parrot <wozeparrot@gmail.com>
2025-03-19 01:05:31 -0400

    feat: cast to correct dtype

--- cv/common/optim.py
+++ cv/common/optim.py
@@ -15,8 +15,8 @@ class CLAMB(Optimizer):
     self.b1_t *= self.b1
     self.b2_t *= self.b2
     for i, (t, g) in enumerate(zip(self.params, grads)):
-      self.m[i].assign(self.b1 * self.m[i] + (1.0 - self.b1) * g)
-      self.v[i].assign(self.b2 * self.v[i] + (1.0 - self.b2) * (g * g))
+      self.m[i].assign((self.b1 * self.m[i] + (1.0 - self.b1) * g).cast(t.dtype))
+      self.v[i].assign((self.b2 * self.v[i] + (1.0 - self.b2) * (g * g)).cast(t.dtype))
       m_hat = self.m[i] / (1.0 - self.b1_t)
       v_hat = self.v[i] / (1.0 - self.b2_t)
       cmask = (m_hat * g > 0).cast(t.dtype)
