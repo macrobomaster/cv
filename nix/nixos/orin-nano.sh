10953f0e2cbb1fde53aeff332dc890793b6922f9
Woze Parrot <wozeparrot@gmail.com>
2025-04-02 04:36:21 -0400

    fix: not -y

--- nix/nixos/orin-nano.sh
+++ nix/nixos/orin-nano.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/macrobomaster/cv
-nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount nix/nixos/disk.nix -y
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount nix/nixos/disk.nix --yes-wipe-all-disks
 nixos-install --flake .#orin-nano --root /mnt
