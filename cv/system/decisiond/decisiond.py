be1991234b64efcc280f31a6fa21301f8f5b05db
Woze Parrot <wozeparrot@gmail.com>
2025-04-06 23:02:50 -0400

    feat: tuning

--- cv/system/decisiond/decisiond.py
+++ cv/system/decisiond/decisiond.py
@@ -164,10 +164,10 @@ def run():
   shoot_decision = ShootDecision()
 
   follower = WaypointFollower([
-    Waypoint(6.2, 0, 6),
-    Waypoint(6.2, 6.2, 6),
-    Waypoint(0, 6, 6),
-    Waypoint(0, 0, 6),
+    Waypoint(6.2, 0, 3),
+    Waypoint(6.2, 6.2, 3),
+    Waypoint(0, 6, 3),
+    Waypoint(0, 0.1, 3),
   ])
 
   fk = FrequencyKeeper(100)
