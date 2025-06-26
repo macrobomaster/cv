45b00a7a8092f7c8cbd5b48da2ecfb74f6d8cf85
Woze Parrot <wozeparrot@gmail.com>
2025-06-26 08:42:56 -0700

    feat: spin when not right color either

--- cv/system/decisiond/decisiond.py
+++ cv/system/decisiond/decisiond.py
@@ -232,9 +232,12 @@ def run():
               chassis_velocity["z"] = -min(CHASE_SPEED, abs(angle_x) / 5)
 
             pub.send("chassis_velocity", chassis_velocity)
+          else:
+            pub.send("shoot", False)
+            pub.send("aim_error", {"x": 0.5, "y": 0.0})
         else:
           pub.send("shoot", False)
-          pub.send("air_error", {"x": 0.5, "y": 0.0})
+          pub.send("aim_error", {"x": 0.5, "y": 0.0})
 
         if autoaim_valid_debounce.debounce(not autoaim["valid"]):
           aim_error_kf.reset()
