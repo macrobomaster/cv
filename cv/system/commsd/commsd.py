7fddf83627609e9c3b5895e6c8f1428178a7a0ee
Woze Parrot <wozeparrot@gmail.com>
2025-06-25 18:04:52 -0700

    feat: use ths0

--- cv/system/commsd/commsd.py
+++ cv/system/commsd/commsd.py
@@ -11,8 +11,8 @@ from .protocol import Protocol, Command, State
 def run():
   kv_put("watchdog", "commsd", time.monotonic())
 
-  if Path("/dev/ttyTHS1").exists():
-    device = "/dev/ttyTHS1"
+  if Path("/dev/ttyTHS0").exists():
+    device = "/dev/ttyTHS0"
   else:
     device = "/dev/ttyUSB0"
   logger.info(f"using device {device}")
