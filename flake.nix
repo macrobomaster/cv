8894e461baee4738de8be19502a5bdcba9eef4f1
Woze Parrot <wozeparrot@gmail.com>
2025-06-25 20:15:51 -0700

    fix: need to pull from packages

--- flake.nix
+++ flake.nix
@@ -309,7 +309,7 @@
                 };
                 script = ''
                   pushd /root/cv
-                  nix develop . --command "bash -c 'JITBEAM=2 python -m cv.system'"
+                  ${pkgs-aarch64-linux}/bin/nix develop . --command "bash -c 'JITBEAM=2 python -m cv.system'"
                 '';
               };
 
