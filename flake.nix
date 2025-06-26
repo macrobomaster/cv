cfd8a19900dab03f776b6ee90134335f74672eae
Woze Parrot <wozeparrot@gmail.com>
2025-06-26 15:53:35 -0700

    feat: name

--- flake.nix
+++ flake.nix
@@ -301,6 +301,7 @@
 
               hardware.deviceTree = {
                 enable = true;
+                name = "tegra234-p3767-0000-super-p3768-0000-a0.dtb";
                 overlays = [
                   {
                     name = "enable-serial";
