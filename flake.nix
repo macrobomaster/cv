2f8d595585c565431ebb867e334336e4a51d2409
Woze Parrot <wozeparrot@gmail.com>
2025-06-25 20:14:43 -0700

    feat: even better

--- flake.nix
+++ flake.nix
@@ -309,7 +309,7 @@
                 };
                 script = ''
                   pushd /root/cv
-                  ${pkgs-aarch64-linux.direnv}/bin/direnv exec env JITBEAM=2 python -m cv.system
+                  nix develop . --command "bash -c 'JITBEAM=2 python -m cv.system'"
                 '';
               };
 
