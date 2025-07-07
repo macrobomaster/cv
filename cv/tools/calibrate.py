86f269afd1695b63051aedf6149684237fe6cd09
Woze Parrot <wozeparrot@gmail.com>
2025-07-06 23:38:00 -0700

    feat: move calibrate to tools

--- cv/tools/calibrate.py
+++ cv/tools/calibrate.py
@@ -1,3 +1,4 @@
+import sys
 import cv2
 import numpy as np
 from ..system.core import messaging
@@ -8,17 +9,19 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*9,3), np.float32)
 objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = objp * 12.7 # scale to 1/2 inch squares but in mm
 
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-sub = messaging.Sub(["camera_feed"], addr="192.168.8.151")
+addr = sys.argv[1]
+sub = messaging.Sub(["camera_feed"], addr=addr)
 
 while len(objpoints) < 20:
   sub.update()
   camera_feed = sub["camera_feed"]
   if sub.updated["camera_feed"] and camera_feed is not None:
-    frame = np.frombuffer(camera_feed, dtype=np.uint8).reshape(256, 512, 3).copy()
+    frame = np.frombuffer(camera_feed["frame"], dtype=np.uint8).reshape(256, 512, 3).copy()
 
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
