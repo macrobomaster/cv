1d2aec3db528ba7c8fd67a38514bbc4aea2ea077
Woze Parrot <wozeparrot@gmail.com>
2026-03-19 01:36:03 -0400

    feat: smaller path

--- cv/system/decisiond/decisiond.py
+++ cv/system/decisiond/decisiond.py
@@ -162,7 +162,7 @@ def _build_path(waypoints: list[tuple[float, float]], speed: float, blend_radius
 
 # -- path config --
 
-PATH_WAYPOINTS = [(0, 0), (7, 0), (7, 1), (-1, 1), (-1, 2), (7, 2), (7, 3), (-1, 3), (-1, 4), (7, 4)]
+PATH_WAYPOINTS = [(0, 0), (6, 0), (6, 1), (0, 1), (0, 2), (6, 2), (6, 3), (0, 3), (0, 4), (6, 4)]
 PATH_SPEED = 1
 PATH_BLEND_RADIUS = 0.5
 
