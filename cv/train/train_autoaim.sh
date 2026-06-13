65bee977c01601db376b3bf1df99156a7e7ef6cc
Woze Parrot <wozeparrot@gmail.com>
2026-06-13 19:09:30 -0400

    feat: remove deprecated flags

--- cv/train/train_autoaim.sh
+++ cv/train/train_autoaim.sh
@@ -6,13 +6,8 @@ export BEAM_UOPS_MAX=4000
 export BEAM_MIN_PROGRESS=5
 export BEAM_UPCAST_MAX=256
 export BEAM_LOCAL_MAX=1024
-export IGNORE_JIT_FIRST_BEAM=1
 export JITBEAM=4
 
-export IGNORE_OOB=1
-export FUSE_ARANGE=1
-# export WINO=1
-# export SINGLE_KERNEL_SOFTMAX=1
 export DEFAULT_FLOAT=float
 
-python -m cv.train
+python3 -m cv.train
