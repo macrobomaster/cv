9441cbdd6ff2e58283df3427e2e45c2e23d9ec99
Woze Parrot <wozeparrot@gmail.com>
2026-06-19 18:24:08 -0400

    feat: more flags

--- nix/nixos/script.sh
+++ nix/nixos/script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 pushd /root/cv || exit
-nix develop . --command env JITBEAM=2 python -m cv.system
+nix develop . --command env JITBEAM=4 JIT_BATCH_SIZE=0 FUSE=1 HALF=1 python -m cv.system
